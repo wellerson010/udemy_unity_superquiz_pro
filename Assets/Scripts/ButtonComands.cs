7e35f2879aa3bf655d3862ea8f9c8b1a29bb5f0f
Wellerson <wellerson.roberto@geopostenergy.com>
2020-04-23 15:17:57 -0300

    Adicionado script de troca de cena

---
+++ Assets/Scripts/ButtonComands.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonComands : MonoBehaviour
+{
+    public void GoToScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
