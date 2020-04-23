6e37e90befd87bb3fb9e8512ad3ab27fb5ba8ef1
Wellerson <wellerson.roberto@geopostenergy.com>
2020-04-23 03:57:34 -0300

    Script background move e adicionado botão sair na tela principal

---
+++ Assets/Scripts/MoveOffset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOffset : MonoBehaviour
+{
+    private Material Material;
+
+    public float VelocityX;
+
+    public float VelocityY;
+
+    public float Increment;
+
+    private float Offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Offset += Increment;
+        Material.SetTextureOffset("_MainTex", new Vector2(Offset * VelocityX, Offset * VelocityY));
+    }
+}
